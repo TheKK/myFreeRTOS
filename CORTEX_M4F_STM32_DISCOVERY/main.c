ec634ca886e6dc70950ae38ba8a63510c03427da
shin21 <abchenry21@hotmail.com>
2014-04-27 22:32:20 +0800

    implement Mutex to check just one task can do something in critical section

--- CORTEX_M4F_STM32_DISCOVERY/main.c
+++ CORTEX_M4F_STM32_DISCOVERY/main.c
@@ -1,44 +1,129 @@
-/**
-  ******************************************************************************
-  * @file    Template/main.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    20-September-2013
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "semphr.h"
 #include <stdio.h>
-/** @addtogroup Template
-  * @{
-  */ 
+#include <stdlib.h>
+#include <string.h>
 
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
+EXTI_InitTypeDef   EXTI_InitStructure;
 
-/* Main Function -------------------------------------------------------------*/
-int main( void )
-{
+void USART(void){
+	/* USART1 clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	/* GPIOA clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/*-------------------------- GPIO Configuration ----------------------------*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Connect USART pins to AF */
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+
+	USART_InitTypeDef USART_InitStructure;
+
+	/* USARTx configuration ------------------------------------------------------*/
+	/* USARTx configured as follow:
+	 *      *  - BaudRate = 9600 baud
+	 *           *  - Word Length = 8 Bits
+	 *                *  - One Stop Bit
+	 *                     *  - No parity
+	 *                          *  - Hardware flow control disabled (RTS and CTS signals)
+	 *                               *  - Receive and transmit enabled
+	 *                                    */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 }
+
+void USART1_puts(char* s){
+	while(*s) {
+		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+		USART_SendData(USART1, *s);
+		s++;
+	}
+}
+
+void itoa(long n, long base) {
+	volatile portCHAR buf[33] = {0};
+	volatile portCHAR *p = &buf[32];
+
+	if (n == 0){
+		*--p = '\r';
+		*--p = '\n';
+		*--p = '0';
+	}
+	else {
+		portCHAR *q;
+		unsigned long num = (base == 10 && n < 0) ? -n : n;
+		*--p = '\r';
+		*--p = '\n';
+		for (; num; num/=base)
+			*--p = "0123456789ABCDEF" [num % base];
+
+		if (base == 10 && n < 0)
+			*--p = '-';
+	}
+
+	USART1_puts( p );
+}
+
+void Init(){
+	STM_EVAL_LEDInit(LED3);
+	STM_EVAL_LEDInit(LED4);
+
+	USART();
+}
+
+
+/*-------------------------- Task ----------------------------*/
+
+SemaphoreHandle_t xMutex = NULL;
+long i = 30;
+
+void Task1( void* pvParameters ){
+	if( xSemaphoreTake( xMutex, portMAX_DELAY ) == pdTRUE ){
+		while( 1 ){
+			i++;
+			itoa(i, 10);
+
+			vTaskDelay(100000);
+		}
+	}
+}
+
+void Task2( void* pvParameters ){
+	while( 1 ){
+		if( xSemaphoreTake( xMutex, portMAX_DELAY ) == pdTRUE ){
+
+			i--;
+			itoa(i, 10);
+
+			xSemaphoreGive( xMutex );
+		}
+
+		vTaskDelay(100000);
+	}
+}
+
+int main( void ){
+	Init();
+	xMutex = xSemaphoreCreateMutex();
+
+	xTaskCreate( Task1, (signed char*)"Task1", 128, NULL, tskIDLE_PRIORITY+1, NULL );
+	xTaskCreate( Task2, (signed char*)"Task2", 128, NULL, tskIDLE_PRIORITY+2, NULL );
+
+	vTaskStartScheduler();
+}
+
+
